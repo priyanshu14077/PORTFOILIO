8afe31f5a60c27236166c1e84eeb0f631d8bba5f
Priyanshu Kumar <priyanshudoingcoding@gmail.com>
2025-05-14 08:51:55 +0530

    Update About section and change contact email address

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -92,7 +92,7 @@ const About = () => {
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">singhpriyanshu11300@gmail.com</p>
+                <p className="lg:text-xl md:text-xl font-medium text-gray_gradient text-white">singhpriyanshu11300@gmail.com</p>
               </div>
             </div>
           </div>
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: 'Priyanshu Kumar Singh',
           from_email: form.email,
-          to_email: 'singhpriyanshu11300@gmail.com',
+          to_email: 'kumarpriyanshu20012@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
